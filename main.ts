01ac628d7d4e93ecd2f25d8764f62d834397c8c2
bianjianhuang <bianjianhuang@huya.com>
2022-04-01 11:32:02 +0800

    添加一个选项modal

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, SuggestModal} from 'obsidian';
 import { addIcon } from 'obsidian';
-import Notion from "notion";
+import { Notion, Database, Page }  from "notion";
 
 
 // Remember to rename these classes and interfaces!
@@ -21,13 +21,12 @@ export default class NotionPlugin extends Plugin {
 	async onload() {
 		console.log("Load Notion.")
 		await this.loadSettings();
+		await this.initNotion()
 		addIcon('Notion', `<svg width="100%" height="100%" viewBox="0 0 25 20" role="img" xmlns="http://www.w3.org/2000/svg"><path d="M4.459 4.208c.746.606 1.026.56 2.428.466l13.215-.793c.28 0 .047-.28-.046-.326L17.86 1.968c-.42-.326-.981-.7-2.055-.607L3.01 2.295c-.466.046-.56.28-.374.466zm.793 3.08v13.904c0 .747.373 1.027 1.214.98l14.523-.84c.841-.046.935-.56.935-1.167V6.354c0-.606-.233-.933-.748-.887l-15.177.887c-.56.047-.747.327-.747.933zm14.337.745c.093.42 0 .84-.42.888l-.7.14v10.264c-.608.327-1.168.514-1.635.514-.748 0-.935-.234-1.495-.933l-4.577-7.186v6.952L12.21 19s0 .84-1.168.84l-3.222.186c-.093-.186 0-.653.327-.746l.84-.233V9.854L7.822 9.76c-.094-.42.14-1.026.793-1.073l3.456-.233 4.764 7.279v-6.44l-1.215-.139c-.093-.514.28-.887.747-.933zM1.936 1.035l13.31-.98c1.634-.14 2.055-.047 3.082.7l4.249 2.986c.7.513.934.653.934 1.213v16.378c0 1.026-.373 1.634-1.68 1.726l-15.458.934c-.98.047-1.448-.093-1.962-.747l-3.129-4.06c-.56-.747-.793-1.306-.793-1.96V2.667c0-.839.374-1.54 1.447-1.632z"/></svg>`);
-
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('Notion', 'Notion', async (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			this.initNotion()
-			await this.notion.getAllDatabase()
+			new DatabaseSelectModal(this.app, this.notion).open()
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -35,43 +34,6 @@ export default class NotionPlugin extends Plugin {
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Notion');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new NotionSettingTab(this.app, this));
@@ -81,7 +43,6 @@ export default class NotionPlugin extends Plugin {
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
 		});
-
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
@@ -89,8 +50,6 @@ export default class NotionPlugin extends Plugin {
 	onunload() {
 
 	}
-
-
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -108,19 +67,37 @@ export default class NotionPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+class DatabaseSelectModal extends SuggestModal<Database>{
+	notion: Notion
+
+	getSuggestions(query: string): Database[] | Promise<Database[]> {
+		return this.notion.getAllDatabase()
+	}
+	renderSuggestion(value: Database, el: HTMLElement) {
+		el.appendText(value.title)
+	}
+	onChooseSuggestion(item: Database, evt: MouseEvent | KeyboardEvent) {
+		throw new Error('Method not implemented.');
+	}
+	constructor(app: App, notion: Notion) {
+		super(app);
+		this.notion = notion
+	}
+
+}
+
+class PageSelectModal extends SuggestModal<Page> {
+	getSuggestions(query: string): Page[] | Promise<Page[]> {
+		throw new Error('Method not implemented.');
+	}
+	renderSuggestion(value: Page, el: HTMLElement) {
+		throw new Error('Method not implemented.');
+	}
+	onChooseSuggestion(item: Page, evt: MouseEvent | KeyboardEvent) {
+		throw new Error('Method not implemented.');
+	}
 	constructor(app: App) {
 		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
--- notion.ts
+++ notion.ts
@@ -1,6 +1,16 @@
 import {requestUrl}  from "obsidian"
+import { isNullOrUndefined } from "util"
 
-export default class Notion  {
+export interface Database {
+    id: string
+    title: string
+    properties: {}
+}
+
+export interface Page {
+}
+
+export class Notion  {
     token: string
     header: { "Authorization": string,  "Notion-Version": string} =  {
         "Authorization": "",
@@ -12,18 +22,44 @@ export default class Notion  {
         this.header["Authorization"] = auth_token
         this.header["Notion-Version"]  = version
     }
+    construct_database_from_list(databases: {id: string, title: {plain_text: string}[], properties: {}}[]): Database[] {
+        let dbs: Database[] = []
+        for( const database of databases) {
+            if ( database.title == undefined || database.title == null || database.title.length < 1) {
+                continue
+            }
+            const db_title = (database.title[0]).plain_text || ''
+            console.log(db_title)
+            dbs.push({title: db_title, id: database.id, properties: database.properties})
+        }
+        return dbs
+    }
 
-	async getAllDatabase() {
+    construct_page_from_list(pages: []): Page {
+        return
+    }
+
+	async getAllDatabase(): Promise<Database[]> {
         const param = {filter: {property: `object`, value:`database`}}
 		const RequestUrlParams = {url:'https://api.notion.com/v1/search', method:'POST', body: JSON.stringify(param), contentType: 'application/json', headers: this.header};
 		const response = await requestUrl(RequestUrlParams)
-		console.log(response)
+        const resp_data = response.json
+        if (resp_data == undefined || resp_data == null) {
+            return []
+        }
+        const databases_list = resp_data.results || []
+        if (!databases_list) {
+            return databases_list
+        }
+        return this.construct_database_from_list(databases_list)
 	}
-	async getAllPage() {
+	async getAllPage(): Promise<Page> {
 		const param = {filter: {property: `object`, value:`page`}}
 		const RequestUrlParams = {url:'https://api.notion.com/v1/search', method:'POST', body: JSON.stringify(param), contentType: 'application/json', headers: this.header};
 		const response = await requestUrl(RequestUrlParams)
-		console.log(response)
+        return
+		//console.log(response)
 	}
 
 }
+
